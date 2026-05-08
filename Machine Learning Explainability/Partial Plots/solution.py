bb2f9f829b30f4f6858a213d4bf784eff5673a93
okk-0 <kulynyakolena@gmail.com>
2026-05-08 19:56:59 +0300

    updated solution of Partial Piots

---
+++ Machine Learning Explainability/Partial Plots/solution.py
@@ -0,0 +1,147 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+
+# Environment Set-Up for feedback system.
+from learntools.core import binder
+binder.bind(globals())
+from learntools.ml_explainability.ex3 import *
+print("Setup Complete")
+
+# Data manipulation code below here
+data = pd.read_csv('../input/new-york-city-taxi-fare-prediction/train.csv', nrows=50000)
+
+# Remove data with extreme outlier coordinates or negative fares
+data = data.query('pickup_latitude > 40.7 and pickup_latitude < 40.8 and ' +
+                  'dropoff_latitude > 40.7 and dropoff_latitude < 40.8 and ' +
+                  'pickup_longitude > -74 and pickup_longitude < -73.9 and ' +
+                  'dropoff_longitude > -74 and dropoff_longitude < -73.9 and ' +
+                  'fare_amount > 0'
+                  )
+
+y = data.fare_amount
+
+base_features = ['pickup_longitude',
+                 'pickup_latitude',
+                 'dropoff_longitude',
+                 'dropoff_latitude']
+
+X = data[base_features]
+
+
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
+first_model = RandomForestRegressor(n_estimators=30, random_state=1).fit(train_X, train_y)
+print("Data sample:")
+data.head()
+
+data.describe()
+
+from matplotlib import pyplot as plt
+from sklearn.inspection import PartialDependenceDisplay
+
+feat_name = 'pickup_longitude'
+PartialDependenceDisplay.from_estimator(first_model, val_X, [feat_name])
+plt.show()
+
+for feat_name in base_features:
+    PartialDependenceDisplay.from_estimator(first_model, val_X, [feat_name])
+
+    plt.show()
+
+fig, ax = plt.subplots(figsize=(8, 6))
+
+# Add your code here
+fig, ax = plt.subplots(figsize=(8, 6))
+fnames = [('pickup_longitude', 'dropoff_longitude')]
+disp = PartialDependenceDisplay.from_estimator(first_model, val_X, fnames, ax=ax)
+plt.show()
+
+savings_from_shorter_trip = 6
+
+# Check your answer
+q_3.check()
+
+# This is the PDP for pickup_longitude without the absolute difference features. Included here to help compare it to the new PDP you create
+feat_name = 'pickup_longitude'
+PartialDependenceDisplay.from_estimator(first_model, val_X, [feat_name])
+plt.show()
+
+# Your code here
+# create new features
+data['abs_lon_change'] = abs(data.dropoff_longitude - data.pickup_longitude)
+data['abs_lat_change'] = abs(data.dropoff_latitude - data.pickup_latitude)
+
+features_2  = ['pickup_longitude',
+               'pickup_latitude',
+               'dropoff_longitude',
+               'dropoff_latitude',
+               'abs_lat_change',
+               'abs_lon_change']
+
+X = data[features_2]
+new_train_X, new_val_X, new_train_y, new_val_y = train_test_split(X, y, random_state=1)
+second_model = RandomForestRegressor(n_estimators=30, random_state=1).fit(new_train_X, new_train_y)
+
+feat_name = 'pickup_longitude'
+disp = PartialDependenceDisplay.from_estimator(second_model, new_val_X, [feat_name])
+plt.show()
+
+# Check your answer
+q_4.check()
+
+import numpy as np
+from numpy.random import rand
+
+n_samples = 20000
+
+# Create array holding predictive feature
+X1 = 4 * rand(n_samples) - 2
+X2 = 4 * rand(n_samples) - 2
+
+# Your code here
+
+# Create y. you should have X1 and X2 in the expression for y
+y = np.ones(n_samples)
+y = -2 * X1 * (X1<-1) + X1 - 2 * X1 * (X1>1) - X2
+# create dataframe 
+my_df = pd.DataFrame({'X1': X1, 'X2': X2, 'y': y})
+predictors_df = my_df.drop(['y'], axis=1)
+
+my_model = RandomForestRegressor(n_estimators=30, random_state=1).fit(predictors_df, my_df.y)
+disp = PartialDependenceDisplay.from_estimator(my_model, predictors_df, ['X1'])
+plt.show()
+
+# Check your answer
+q_6.check()
+
+import eli5
+from eli5.sklearn import PermutationImportance
+
+n_samples = 20000
+
+# Create array holding predictive feature
+X1 = 4 * rand(n_samples) - 2
+X2 = 4 * rand(n_samples) - 2
+# Create y. you should have X in the expression for y
+y = X1 * X2
+
+
+# create dataframe because pdp_isolate expects a dataFrame as an argument
+my_df = pd.DataFrame({'X1': X1, 'X2': X2, 'y': y})
+predictors_df = my_df.drop(['y'], axis=1)
+
+my_model = RandomForestRegressor(n_estimators=30, random_state=1).fit(predictors_df, my_df.y)
+
+
+disp = PartialDependenceDisplay.from_estimator(my_model, predictors_df, ['X1'], grid_resolution=300)
+plt.show()
+
+perm = PermutationImportance(my_model).fit(predictors_df, my_df.y)
+
+# Check your answer
+q_7.check()
+
+# show the weights for the permutation importance you just calculated
+eli5.show_weights(perm, feature_names = ['X1', 'X2'])
+
